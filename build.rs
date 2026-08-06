dc659ac4059fde43c13a8be730d6672d0f6855c3
CabinetOnFire <>
2026-08-06 22:57:13 +0200

    missed

--- build.rs
+++ build.rs
@@ -1,17 +1,9 @@
-use std::{fs, io, path::Path};
-
-fn append_file(output: &mut String, path: impl AsRef<Path>) -> io::Result<()> {
-    output.push_str(&fs::read_to_string(path)?);
-    output.push('\n');
-    Ok(())
-}
+use std::{fs, io};
 
 fn main() -> io::Result<()> {
     println!("cargo:rerun-if-changed=dmsrc/navmap_pathfinder.dm");
 
-    let mut dm = String::new();
-    append_file(&mut dm, "dmsrc/navmap_pathfinder.dm")?;
-
     fs::create_dir_all("target")?;
+    let dm = fs::read_to_string("dmsrc/navmap_pathfinder.dm")?;
     fs::write("target/navmap_pathfinder.dm", dm)
 }
